d461652921f73067c17cdadede256832be9c0ab4
NareshNalla <nrs24@live.com>
2018-01-04 09:12:29 +0530

    code to push

--- MyProjectOrg/WebContent/WEB-INF/js/website.js
+++ MyProjectOrg/WebContent/WEB-INF/js/website.js
@@ -18,7 +18,7 @@ angular.module('website', ['ngRoute']).
        
     })
    
-    .controller('HomeCtrl', function ($scope, StateService) {
+    .controller('HomeCtrl', function ($scope) {
     	
         $scope.title = 'Home Page';
         $scope.body = 'This is the about home body';
@@ -31,15 +31,75 @@ angular.module('website', ['ngRoute']).
         
     })
     
-    .controller('regiCtrl', function ($scope, StateService, ExperimentsService) {
+    .controller('regiCtrl', function ($scope,$http) {
         $scope.title = 'Registration';
         console.log("in regi");
-        
+        alert("abcde");
         $scope.gallerybottom = false;
         $scope.logobottom = false;
-    })
-     
- 
+        $scope.submit();
+        $scope.submit = function() {
+        	$scope.firstName = null;
+            $scope.lastName = null;
+            $scope.gender = null;
+            $scope.email_adr = null;
+            $scope.mobl_nm = null;
+            $scope.occupation = null;
+            $scope.colg_nm = null;
+            $scope.colg_join_year = null;
+            $scope.adr_ln1 = null;
+            $scope.adr_ln2 = null;
+            $scope.district = null;
+            $scope.state = null;
+            $scope.univ_nm = null;
+            alert("ccc"+$scope.univ_nm)
+            $scope.postdata();
+            $scope.postdata = function (firstName, lastName, gender,email_adr,mobl_nm,
+            		occupation,colg_nm,colg_join_year,adr_ln1,adr_ln2,district,state,univ_nm) {
+            	 alert("firstName: in postData"+firstName)
+            	var data = {
+            			firstName: firstName,
+            			lastName: lastName,
+            			gender: gender,
+            			email_adr: email_adr,
+            			mobl_nm: mobl_nm,
+            			occupation: occupation,
+            			colg_nm: colg_nm,
+            			colg_join_year: colg_join_year,
+            			adr_ln1: adr_ln1,
+            			adr_ln2: adr_ln2,
+            			district: district,
+            			state: state,
+            			univ_nm: univ_nm
+            			
+            	};
+
+            	//Call the services
+            	$http.post('http://52.66.176.19:8080/hiber/#/register', JSON.stringify(data)).then(function (response) {
+
+            		if (response.data)
+
+            			$scope.msg = "Post Data Submitted Successfully!";
+            		alert("sucess"+$scope.msg);
+
+            	}, function (response) {
+
+            		$scope.msg = "Service not Exists";
+            		alert("Fail"+$scope.msg);
+
+            		$scope.statusval = response.status;
+
+            		$scope.statustext = response.statusText;
+
+            		$scope.headers = response.headers();
+
+            	});
+
+            };
+
+        };
+        
+        })
     .factory('StateService', function () {
         var message = 'Hello Message';
         var getMessage = function () {
